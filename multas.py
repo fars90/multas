a21526ebc666bdd43d7a66bc07ca88ca3b73b0f8
fars90 <andreraposeirofaria@gmail.com>
2025-07-03 13:47:29 +0100

    Update multas.py

--- multas.py
+++ multas.py
@@ -1,12 +1,37 @@
 def localidade():
-    
+    velocidade = int(input("Introduza a Velocidade do Veículo (em km/h): "))
+    print()
+    if velocidade > 50:
+        print("A multa é de 60€ !")
+        return
+    if velocidade >= 90:
+        print("A multa é de 120€ !")
+        return
+    if velocidade >= 120:
+        print("A multa é de 320€ !")
+        return
+    else:
+        print("Não estava em excesso de velocidade!")
+
 while True:
-    print("Bem Vindo!\n")
+    print(" ---------- ")
+    print("|Bem Vindo!|")
+    print(" ---------- ")
     print("Onde circulava o veículo?")
     print("Escolha uma opção: ")
     print("1 - Localidade")
     print("2 - Fora da Localidade")
     print("3 - Autoestrada")
+    print("0 - Sair")
     print()
-    opcao = input("Opção:")
+   
+    opcao = input("Opção: ")
+    print()
     
+    if opcao == "1":
+        localidade()
+    elif opcao == "0":
+        print("Obrigado!")
+        break
+    else:
+        print("Opção invalida!")
