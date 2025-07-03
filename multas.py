0fbec7720017b5545f49ad83ad2258f6848e2046
fars90 <andreraposeirofaria@gmail.com>
2025-07-03 13:30:59 +0100

    Create multas.py

---
+++ multas.py
@@ -0,0 +1,12 @@
+def localidade():
+    
+while True:
+    print("Bem Vindo!\n")
+    print("Onde circulava o veículo?")
+    print("Escolha uma opção: ")
+    print("1 - Localidade")
+    print("2 - Fora da Localidade")
+    print("3 - Autoestrada")
+    print()
+    opcao = input("Opção:")
+    
